512248dd7b51271ef495165ecf14896ef54d0fa1
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-08 13:13:41 +0000

    Use Tuple from typing instead of builtin tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,4 +6,12 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''
+    Builds a tuple which first member is a string and the last a number
+    Arg:
+        k: a string
+        v: an integer or float
+    Return:
+        a tuple composed by the @k and @v
+    '''
     return k, v * v
