233a9717fcf014ecf00f1184832632c0ab4633c5
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-08 13:19:52 +0000

    Comment out the function

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -7,6 +7,13 @@ from typing import Sequence, Union, Any
 
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''
+    Get the first element of a sequence passed in
+    Arg:
+        lst: any kind of sequence
+    Return:
+        the first sequence element if found, or None
+    '''
     if lst:
         return lst[0]
     else:
