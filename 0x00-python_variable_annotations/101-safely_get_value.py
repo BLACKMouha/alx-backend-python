94a7621cf865e82cad8698ffee9765bfb9bd1b2b
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-08 13:24:46 +0000

    Commenting out the function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,7 +2,7 @@
 
 '''101-safely_get_value module'''
 
-from typing import Sequence, Union, Any, Mapping, TypeVar
+from typing import Union, Any, Mapping, TypeVar
 
 T = TypeVar('T', bound=Any)
 
@@ -10,6 +10,16 @@ T = TypeVar('T', bound=Any)
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None
                      ) -> Union[Any, T]:
+    '''
+    Gets the value of dictionary key if found otherwise the default value
+    Arg:
+        dict: a mapping object
+        key: a mapping key of any type
+        default: a T or NoneType object
+    Return:
+        The value of at the key dictionary if found, or the default value
+        passed in
+    '''
     if key in dct:
         return dct[key]
     else:
