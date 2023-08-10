365c079459781c38aa4bb20103f6e4335aba4b1e
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-10 13:53:10 +0000

    Measures the runtime of a async_comprehension call

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ from typing import List
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> List[float]:
+async def measure_runtime() -> float:
     '''
     Measures the runtime of a async_comprehension call
     '''
