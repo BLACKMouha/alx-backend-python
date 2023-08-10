433ff99c70f0a1a8661c2faa84baa9f8a2975897
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-10 13:23:19 +0000

    Builds an asynchronous list comprehension of random float numbers     between 0 to 10

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,12 +4,12 @@
 
 import asyncio
 import random
-from typing import AsyncIterator
+from typing import Iterator
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> AsyncIterator[float]:
+async def async_comprehension() -> Iterator[float]:
     '''
     Builds an asynchronous list comprehension of random float numbers
     between 0 to 10
