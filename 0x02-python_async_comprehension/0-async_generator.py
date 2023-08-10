0558c2191f8bf6dd54670f566d46453d07e73650
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-10 12:54:28 +0000

    Yields a random float number between 0 to 10 and sleeps for 1 second

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,10 +7,10 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     '''
     Yields a random float number between 0 to 10 and sleeps for 1 second
     '''
-    for i in range(10):
+    for _ in range(10):
         yield random.uniform(0, 10)
         await asyncio.sleep(1)
