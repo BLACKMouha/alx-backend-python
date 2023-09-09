c080624235f475e63c7b61d5fc6dac635f15d5ee
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-09 07:59:21 +0000

    Unittest for testing if KeyError is raised by access_nested_map method in some contexts execution

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,6 +16,14 @@ class TestAccessNestedMap(unittest.TestCase):
         '''Checks if the access_nested_map returns the correct output'''
         self.assertEqual(access_nested_map(a, b), e)
 
+    @parameterized.expand([
+        ({}, ('a', )),
+        ({'a': 1}, ('a', 'b'))
+    ])
+    def test_access_nested_map_exception(self, a, b):
+        '''Checks if a KeyError is raised in the following cases'''
+        self.assertRaises(KeyError, access_nested_map, a, b)
+
 
 if __name__ == '__main__':
     unittest.main()
