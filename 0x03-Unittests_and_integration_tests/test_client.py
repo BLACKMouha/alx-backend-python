a80598ec5a7645d457642e75cd523a57b16236f0
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-11 19:05:45 +0000

    Unittest for client.GithubClient._public_repos_url property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -25,7 +25,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @parameterized.expand([
         ('google', {'repos_url': 'https://api.github.com/orgs/google/repos'}),
-        ('abc', {'mesage': 'Not found!'})
+        ('abc', {'message': 'Not found!'})
     ])
     def test_public_repos_url(self, org_name, response):
         '''Test cases for GithubOrgClient._public_repos_url'''
@@ -34,8 +34,8 @@ class TestGithubOrgClient(unittest.TestCase):
             goc = GithubOrgClient(org_name=org_name)
             url = goc.ORG_URL.format(org=org_name)
             mock.return_value = response
-            if 'repos_url' not in response:
-                self.assertRaises(KeyError('this organization does not exist'))
-            else:
+            if 'repos_url' in response:
                 self.assertEqual(response['repos_url'], goc._public_repos_url)
+            else:
+                self.assertRaises(KeyError, msg='this organization does not exist')
             return
