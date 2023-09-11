20a8da2a4d9373a02755a37550b7bc82f7f3c1c8
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-11 20:45:49 +0000

    Unittest for client.GithubClient.public_repos_url method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -24,19 +24,30 @@ class TestGithubOrgClient(unittest.TestCase):
         return
 
     @parameterized.expand([
-        ('google', {'repos_url': 'https://api.github.com/orgs/google/repos'}),
-        ('abc', {'message': 'Not found!'})
+        (
+            'google',
+            {'repos_url': 'https://api.github.com/orgs/google/repos'},
+            ['episodes.dart', 'cpp-netlib']
+        ),
+        (
+            'abc',
+            {'message': 'Not found!'},
+            []
+        )
     ])
-    def test_public_repos_url(self, org_name, response):
-        '''Test cases for GithubOrgClient._public_repos_url'''
-        with patch('client.GithubOrgClient.org',
-                   new_callable=PropertyMock) as mock:
+    @patch('client.get_json', new_callable=Mock)
+    def test_public_repos_url(self, org_name, response, repos, mock_get_json):
+        '''Test cases for GithubOrgClient._public_repos_url and
+        GihubOrgClient.public_repos_url'''
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_pru:
             goc = GithubOrgClient(org_name=org_name)
             url = goc.ORG_URL.format(org=org_name)
-            mock.return_value = response
             if 'repos_url' in response:
+                mock_pru.return_value = response['repos_url']
+                mock_get_json(response['repos_url']).return_value = repos
                 self.assertEqual(response['repos_url'], goc._public_repos_url)
             else:
                 self.assertRaises(KeyError,
-                                  msg='this organization does not exist')
+                                  msg=response['message'])
         return
