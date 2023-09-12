cfb1a97e0e9d85845ab99d432b384b889838d18c
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-12 08:54:43 +0000

    Unittest for client.GithubClient.public_repos_url method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -44,27 +44,41 @@ class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand([
         (
             'google',
-            {'repos_url': 'https://api.github.com/orgs/google/repos'},
+            {
+                'repos_url': 'https://api.github.com/orgs/google/repos',
+                'repos': [
+                    {
+                        "id": 7697149,
+                        "name": "episodes.dart",
+                    },
+                    {
+                        "id": 7776515,
+                        "name": "cpp-netlib",
+                    }
+                ]
+            },
             ['episodes.dart', 'cpp-netlib']
         ),
         (
             'abc',
-            {'message': 'Not found!'},
+            {'message': 'Not Found'},
+            [KeyError, 'repos_url']
         )
     ])
     @patch('client.get_json', new_callable=Mock)
-    def test_public_repos(self, org_name, response, repos, mock_get_json):
-        '''Test cases for GithubOrgClient._public_repos_url and
+    def test_public_repos(self, org_name, response, public_repos, mock_get_json):
+        '''Test cases for GithubOrgClient.public_repos and
         GihubOrgClient.public_repos_url'''
         with patch('client.GithubOrgClient._public_repos_url',
                    new_callable=PropertyMock) as mock_pru:
-            goc = GithubOrgClient(org_name=org_name)
-            url = goc.ORG_URL.format(org=org_name)
+            goc = GithubOrgClient(org_name)
             if 'repos_url' in response:
                 mock_pru.return_value = response['repos_url']
-                mock_get_json(response['repos_url']).return_value = repos
-                self.assertEqual(response['repos_url'], goc._public_repos_url)
+                mock_get_json.return_value = response['repos']
+                self.assertEqual(goc._public_repos_url, response['repos_url'])
+                self.assertEqual(goc.public_repos(),
+                                 public_repos)
+                mock_get_json.assert_called_once_with(response['repos_url'])
             else:
-                self.assertRaises(KeyError,
-                                  msg=response['message'])
-        return
+                self.assertRaises(KeyError, msg='repos_url')
+            
